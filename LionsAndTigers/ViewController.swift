8cd8586c6cfab81e950f9ec05c4efb3a500543b7
kenwilcox <wilcoxk@gmail.com>
2014-09-15 08:42:05 -0600

    More Tigers

--- LionsAndTigers/ViewController.swift
+++ LionsAndTigers/ViewController.swift
@@ -21,11 +21,32 @@ class ViewController: UIViewController {
     
     var myTiger = Tiger()
     myTiger.name = "Tigger"
-    myTiger.breed = "Bengal"
+    myTiger.breed = "Bengal Tiger"
     myTiger.age = 3
     myTiger.image = UIImage(named:"BengalTiger.jpg")
     
-    println("My Tiger's name is: \(myTiger.name), it's age is \(myTiger.age), its' breed is \(myTiger.breed), and the image is \(myTiger.image)")
+    imageView.image = myTiger.image
+    nameLabel.text = myTiger.name
+    ageLabel.text = "\(myTiger.age)"
+    breedLabel.text = myTiger.breed
+    
+    var secondTiger = Tiger()
+    secondTiger.name = "Tigress"
+    secondTiger.breed = "Indochinese Tiger"
+    secondTiger.age = 2
+    secondTiger.image = UIImage(named:"IndochineseTiger.jpg")
+    
+    var thirdTiger = Tiger()
+    thirdTiger.name = "Jacob"
+    thirdTiger.breed = "Malayan Tiger"
+    thirdTiger.age = 4
+    thirdTiger.image = UIImage(named:"MalayanTiger.jpg")
+    
+    var fourthTiger = Tiger()
+    fourthTiger.name = "Spar"
+    fourthTiger.breed = "Siberian Tiger"
+    fourthTiger.age = 5
+    fourthTiger.image = UIImage(named:"SiberianTiger.jpg")
   }
 
   override func didReceiveMemoryWarning() {
