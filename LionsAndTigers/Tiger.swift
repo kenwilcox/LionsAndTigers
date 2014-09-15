131f54675077a50648785bd9779914b3cb71fa04
kenwilcox <wilcoxk@gmail.com>
2014-09-15 08:12:00 -0600

    Added Tiger

---
+++ LionsAndTigers/Tiger.swift
@@ -0,0 +1,13 @@
+//
+//  Tiger.swift
+//  LionsAndTigers
+//
+//  Created by Kenneth Wilcox on 9/15/14.
+//  Copyright (c) 2014 Kenneth Wilcox. All rights reserved.
+//
+
+import Foundation
+
+struct Tiger {
+  
+}
