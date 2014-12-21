32c10644181a248e746119e2fa7913b048e8db7e
kenwilcox <wilcoxk@gmail.com>
2014-12-20 20:56:10 -0700

    Added Lion class

---
+++ LionsAndTigers/Lion.swift
@@ -0,0 +1,18 @@
+//
+//  Lion.swift
+//  LionsAndTigers
+//
+//  Created by Kenneth Wilcox on 12/20/14.
+//  Copyright (c) 2014 Kenneth Wilcox. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Lion {
+  var age = 0
+  var isAlphaMale = false
+  var image = UIImage(named:"")
+  var name = ""
+  var subspecies = ""
+}
